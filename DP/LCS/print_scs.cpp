12653ce50e4b3435424b77334d910489f98d3418
kundannitt <125723392+kundannitt@users.noreply.github.com>
2023-08-13 08:06:37 +0000

    print SCS

---
+++ DP/LCS/print_scs.cpp
@@ -0,0 +1,52 @@
+string shortestCommonSupersequence(string str1, string str2) {
+    int m = str1.size();
+    int n = str2.size();
+    //LCS-----------------------------------------------
+    vector<vector<int>>dp(m+1,vector<int>(n+1));
+    for(int i=0;i<=m;i++){
+      dp[i][0] = 0;
+    }
+    for(int j=0;j<=n;j++){
+      dp[0][j] = 0;
+    }
+    for(int i=1;i<=m;i++){
+      for(int j=1;j<=n;j++){
+        if(str1[i-1] == str2[j-1]){
+          dp[i][j] = 1 + dp[i-1][j-1];
+        }else{
+          dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+        }
+      }
+    }
+    //-----------------------------------------------------
+    string ans = "";
+    int i = m,j=n;
+    while(i>0 && j>0){
+      if(str1[i-1] == str2[j-1]){
+        ans.push_back(str1[i-1]);
+        i--;
+        j--;
+      }else{
+        if(dp[i][j-1] > dp[i-1][j]){
+          // ans.push_back(str1[i-1]);
+          ans.push_back(str2[j-1]);
+          j--;
+        }else{
+          ans.push_back(str1[i-1]);
+          // ans.push_back(str2[j-1]);
+          i--;
+        }
+      }
+    }
+    while(i>0){
+      // cout<<str1[i-1]<<" ";
+      ans.push_back(str1[i-1]);
+      i--;
+    }
+    while(j>0){
+      ans.push_back(str2[j-1]);
+      j--;
+    }
+    reverse(ans.begin(),ans.end());
+    return ans;
+  }
