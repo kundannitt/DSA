599d158cb64e6831cd1296b1134e0603b2c406bb
kundannitt <125723392+kundannitt@users.noreply.github.com>
2023-08-14 18:43:58 +0000

    .

---
+++ DP/MCM/palindrome_partitioning_rec.cpp
@@ -0,0 +1,36 @@
+//return min number of partition needed such that each partition is a palindrome.
+#include <bits/stdc++.h>
+using namespace std;
+
+bool ispal(string s ,int i,int j){
+  while(i<=j){
+    if(s[i] != s[j]){
+      return false;
+    }
+    i++;
+    j--;
+  } 
+  return true;
+}
+
+int partition(string s ,int i,int j){
+  if(i>=j){ // i>j => size is 0, i==j => size = 1, in both cases we need 0 partition.
+    return 0;
+  }
+  if(ispal(s,i,j) == true){
+    return 0;
+  }
+  int ans = INT_MAX;
+  for(int k=i;k<j;k++){
+    int tempans = partition(s,i,k) + partition(s,k+1,j) + 1;
+    ans = min(ans,tempans);
+  }
+  return ans;
+}
+
+int main(){
+  string s;
+  cin>>s;
+  cout<<partition(s,0,s.size()-1);
+  return 0;
+}
