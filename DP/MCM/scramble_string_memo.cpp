3e527d96511a66810af3a2953c4f21deb3fd55e7
kundannitt <125723392+kundannitt@users.noreply.github.com>
2023-08-15 11:39:10 +0000

    .

--- DP/MCM/scramble_string_memo.cpp
+++ DP/MCM/scramble_string_memo.cpp
@@ -1,43 +1,48 @@
-class Solution {
-public:
-  unordered_map<string,int>mp;
-  bool isScramble(string s1, string s2) {
-    if(s1==s2){
-      return true;
-    }
-    if(s1.empty() && s2.empty()){
-      return true;
-    }
-    if(s1.size() != s2.size()){
-      return false;
-    }
-    return solve(s1,s2);
+#include<bits/stdc++.h>
+using namespace std;
+unordered_map<string,int>mp;
+bool solve(string s,string t){
+  if(s==t){
+    return true;
   }
-  bool solve(string s,string t){
-    if(s==t){
-      return true;
-    }
-    if(s.size() <= 1){
-      return false;
-    }
-    string temp = s;
-    temp.push_back(' ');
-    temp += t;
-    if(mp.find(temp) != mp.end()){
-      return mp[temp];
-    }
-    int n = s.size();
-    bool ans = false;
-    for(int i=1;i<n;i++){
-      bool con1 = solve(s.substr(0,i),t.substr(n-i,i)) 
-               && solve(s.substr(i,n-i),t.substr(0,n-i));
-      bool con2 = solve(s.substr(0,i),t.substr(0,i)) 
-               && solve(s.substr(i,n-i),t.substr(i,n-i));
-      if(con1 || con2 == true){
-        ans = true;
-        break;
-      }
-    }
-    return mp[temp] = ans;
+  if(s.size() <= 1){
+    return false;
   }
-};
+  string temp = s;
+  temp.push_back(' ');
+  temp += t;
+  if(mp.find(temp) != mp.end()){
+    return mp[temp];
+  }
+  int n = s.size();
+  bool ans = false;
+  for(int i=1;i<n;i++){
+    bool con1 = solve(s.substr(0,i),t.substr(n-i,i)) 
+              && solve(s.substr(i,n-i),t.substr(0,n-i));
+    bool con2 = solve(s.substr(0,i),t.substr(0,i)) 
+              && solve(s.substr(i,n-i),t.substr(i,n-i));
+    if(con1 || con2 == true){
+      ans = true;
+      break;
+    }
+  }
+  return mp[temp] = ans;
+}
+bool isScramble(string s1, string s2) {
+  if(s1==s2){
+    return true;
+  }
+  if(s1.empty() && s2.empty()){
+    return true;
+  }
+  if(s1.size() != s2.size()){
+    return false;
+  }
+  return solve(s1,s2);
+}
+int main(){
+  string s,t;
+  cin>>s>>t;
+  cout<<isScramble(s,t);
+  return 0;
+}
