34fba963abb0d8f9b2965404e1fdec3e01b2a063
kundannitt <kundankumarnitt@gmail.com>
2023-10-01 21:42:41 +0530

    graph

--- Graph_Striver/bellman_ford.cpp
+++ Graph_Striver/bellman_ford.cpp
@@ -1,79 +1,82 @@
-//{ Driver Code Starts
 #include <bits/stdc++.h>
 using namespace std;
-
-// } Driver Code Ends
-// User function Template for C++
-
-class Solution {
+//works on both directed and undirected.
+//single source shortest path but it works with negative weights also, because we fixed the number of relaxtion to n-1
+class Solution{
   public:
-    /*  Function to implement Bellman Ford
-    *   edges: vector of vectors which represents the graph
-    *   S: source vertex to start traversing graph with
-    *   V: number of vertices
-    */
-    vector<int> bellman_ford(int V, vector<vector<int>>& edges, int S) {
-        // Code here
-        vector<int>dist(V,1e8);
-        dist[S] = 0;
-        
-        for(int i=0;i<V-1;i++){
-            for(auto it : edges){
-                int u = it[0];
-                int v = it[1];
-                int wt = it[2];
-                if(dist[u] != 1e8 && dist[u] + wt < dist[v]){
-                    dist[v] = wt + dist[u];
-                } 
-            } 
+  vector<int>bellman_ford(int n,vector<vector<int>>&edges,int src){
+    vector<int>dist(n,INT_MAX);
+    dist[src] = 0;
+    for(int i=0;i<n-1;i++){
+      for(auto it:edges){
+        int u = it[0];
+        int v = it[1];
+        int wt = it[2];
+        if(dist[u] != INT_MAX && dist[u] + wt < dist[v]){
+          dist[v] = wt + dist[u];
         }
-        //check if edge can be relaxed more
-        // if it is again relaxable then it contain negative cycle
-        for(auto it:edges){
-            int u = it[0];
-            int v = it[1];
-            int wt = it[2];
-            if(dist[u] != 1e8 && dist[u] + wt < dist[v]){
-                return {-1};
-            }
-        }
-        return dist;
+      }
     }
+    /*
+      detect negative cycle using this algo
+      1. we assume that after n-1th iteration no more edges can be relaxed
+      2. So, we try to relax the edges one more time and if any edge can be relexed then the graph contains neg cycle.
+
+      for(auto it:edges){
+        int u = it[0];
+        int v = it[1];
+        int wt = it[2];
+        if(dist[u] != INT_MAX && dist[u] + wt < dist[v]){
+          since entered here => edges can be relaxed again => neg cycle.
+          return {-1};
+        }
+      }
+
+    */
+    return dist;
+  }
 };
 
-
-//{ Driver Code Starts.
-
 int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int N, m;
-        cin >> N >> m;
-        vector<vector<int>> edges;
+  int N, m;
+  cin >> N >> m;
+  vector<vector<int>> edges;
 
-        for (int i = 0; i < m; ++i) {
-            vector<int> temp;
-            for (int j = 0; j < 3; ++j) {
-                int x;
-                cin >> x;
-                temp.push_back(x);
-            }
-            edges.push_back(temp);
-        }
-
-        int src;
-        cin >> src;
-
-        Solution obj;
-        vector<int> res = obj.bellman_ford(N, edges, src);
-
-        for (auto x : res) {
-            cout << x << " ";
-        }
-        cout << "\n";
+  for (int i = 0; i < m; ++i) {
+    vector<int> temp;
+    for (int j = 0; j < 3; ++j) {
+      int x;
+      cin >> x;
+      temp.push_back(x);
     }
-    return 0;
+    edges.push_back(temp);
+  }
+
+  int src;
+  cin >> src;
+
+  Solution obj;
+  vector<int> res = obj.bellman_ford(N, edges, src);
+
+  for (auto x : res) {
+    cout << x << " ";
+  }
+  cout << "\n";
+  return 0;
 }
 
-// } Driver Code Ends
+/*
+  sample input:
+    6
+    7
+    0 1 5
+    1 2 -2
+    1 5 -3
+    5 3 1
+    3 2 6
+    3 4 -2
+    2 3 4
+    0
+  sample output :
+    0 5 3 3 1 2 
+*/
--- Graph_Striver/floydWarshall.cpp
+++ Graph_Striver/floydWarshall.cpp
@@ -1,68 +1,67 @@
-//{ Driver Code Starts
-//Initial template for C++
-
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+// it tells us shortest path from any node to any other node in a matrix.
+// apply algorithm in adjacency matrix.
+// works on both directed and undirected
 
-// } Driver Code Ends
-//User function template for C++
-
-class Solution {
+class Solution{
   public:
-	void shortest_distance(vector<vector<int>>&matrix){
-	    // Code here
-	    int n = matrix.size();
-	    
-	    for(int i=0;i<n;i++){
-	        for(int j=0;j<n;j++){
-	            if(matrix[i][j] == -1){
-	                matrix[i][j] = 1e9;
-	            }
-	            if(i == j){
-	                matrix[i][j] = 0;
-	            }
-	        }
-	    }
-	    
-	    for(int via = 0; via<n; via++){
-	        for(int i=0;i<n;i++){
-	            for(int j=0; j<n; j++){
-	                matrix[i][j] = min(matrix[i][j],matrix[i][via] + matrix[via][j]);
-	            }
-	        }
-	    }
-	    for(int i=0;i<n;i++){
-	        for(int j=0;j<n;j++){
-	            if(matrix[i][j] == 1e9){
-	                matrix[i][j] = -1;
-	            }
-	        }
-	    }
-	}
+  void floyd(vector<vector<int>>&adjMat,int n){
+    // we go via every node to every node => time :- O(n^3)
+    for(int via = 0;via<n;via++){
+      for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+          adjMat[i][j] = min(adjMat[i][j] , adjMat[i][via] + adjMat[via][j]);
+        }
+      }
+    }
+    for(int i=0;i<n;i++){
+      for(int j=0;j<n;j++){
+        if(adjMat[i][j] == 1e9){
+          adjMat[i][j] = -1;
+        }
+      }
+    }
+  }
 };
 
-//{ Driver Code Starts.
 int main(){
-	int tc;
-	cin >> tc;
-	while(tc--){
-		int n;
-		cin >> n;
-		vector<vector<int>>matrix(n, vector<int>(n, -1));
-		for(int i = 0; i < n; i++){
-			for(int j = 0; j < n; j++){
-				cin >> matrix[i][j];
-			}
-		}
-		Solution obj;
-		obj.shortest_distance(matrix);
-		for(int i = 0; i < n; i++){
-			for(int j = 0; j < n; j++){
-				cout << matrix[i][j] << " ";
-			}
-			cout << "\n";
-		}
-	}
-	return 0;
+  int n,m;
+  cin>>n>>m;
+  vector<vector<int>>adjMat(n,vector<int>(n,1e9));
+  for(int i=0;i<m;i++){
+    int u,v,wt;
+    cin>>u>>v>>wt;
+    adjMat[u][v] = wt;
+  }
+  for(int i=0;i<n;i++){
+    adjMat[i][i] = 0;
+  }
+  Solution obj;
+  obj.floyd(adjMat,n);
+  for(auto i:adjMat){
+    for(auto j:i){
+      cout<<j<<" ";
+    }
+    cout<<endl;
+  }
+  return 0;
 }
-// } Driver Code Ends
+
+/*
+  Sample Input:-
+    4
+    6
+    0 1 3
+    1 0 2
+    2 1 1
+    3 2 2
+    1 3 4
+    0 3 5
+    
+  Sample Output:-
+    0 3 7 5 
+    2 0 6 4 
+    3 1 0 5 
+    5 3 2 0
+*/
