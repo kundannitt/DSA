3ab152edc727f1855f9c59c61e5b38fd6a6830a7
kundannitt <kundankumarnitt@gmail.com>
2024-03-18 12:27:26 +0530

    gcd

---
+++ Maths/gcd.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+//euclidean
+int gcd(int n, int m){
+  if (m==0)
+  return n;
+  else return gcd(m,n%m);
+}
+
+int main(){
+  int n,m;
+  cout<<"Enter two numbers : ";
+  cin>>n>>m;
+  cout<<gcd(m,n);
+  return 0;
+}
